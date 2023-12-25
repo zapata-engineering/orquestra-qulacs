eb40b3e7ac6fb67501b120f3fdb61885f18afa92
Shiro-Raven <exclass9.24@gmail.com>
2023-12-25 20:18:29 +0100

    moved test SU4 gate code to the test file

--- tests/orquestra/integrations/qulacs/simulator_test.py
+++ tests/orquestra/integrations/qulacs/simulator_test.py
@@ -2,9 +2,8 @@
 # © Copyright 2021-2022 Zapata Computing Inc.
 ################################################################################
 import numpy as np
-import sympy
-
 import pytest
+import sympy
 from orquestra.quantum.api.circuit_runner_contracts import (
     CIRCUIT_RUNNER_CONTRACTS,
     STRICT_CIRCUIT_RUNNER_CONTRACTS,
@@ -16,15 +15,44 @@ from orquestra.quantum.api.wavefunction_simulator_contracts import (
 )
 from orquestra.quantum.circuits import (
     Circuit,
+    CustomGateDefinition,
     H,
     MultiPhaseOperation,
     X,
-    CustomGateDefinition,
+)
+from orquestra.quantum.circuits._builtin_gates import make_parametric_gate_prototype
+from orquestra.quantum.circuits._matrices import (
+    u3_matrix,
+    xx_matrix,
+    yy_matrix,
+    zz_matrix,
 )
 
 from orquestra.integrations.qulacs.simulator import QulacsSimulator
 
-from _su4 import SU4
+
+def _su4(angles: np.ndarray) -> np.ndarray:
+    """
+    A simplified implementation of SU4 gates based on that from
+    ```orquestra.qml.models.qcbm.gate_factories.su4_factories```
+    for testing purposes, with jax.numpy replaced with numpy
+    """
+    angle_scaling = np.array([2, 1, 1, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 1, 1])
+
+    angles = angles * angle_scaling
+    return (
+        np.kron(u3_matrix(*angles[9:12]), u3_matrix(*angles[12:15]))
+        .dot(
+            np.dot(
+                np.dot(zz_matrix(angles[8]), yy_matrix(angles[7])),
+                xx_matrix(angles[6]),
+            )
+        )
+        .dot(np.kron(u3_matrix(*angles[0:3]), u3_matrix(*angles[3:6])))
+    )
+
+
+SU4 = make_parametric_gate_prototype("SU4", _su4, 2)
 
 
 @pytest.fixture
